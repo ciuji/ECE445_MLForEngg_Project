f4c5f5a9e94fd9dbdff7bd3c38a21dbd02a00195
ciuji's mbp <zuochaoji@gmail.com>
2018-12-11 13:09:18 -0500

    [modify]BF

--- black-friday/pre_blackfriday.py
+++ black-friday/pre_blackfriday.py
@@ -6,10 +6,11 @@ Created on Mon Dec 10 16:19:09 2018
 @author: ciuji
 """
 
-import numpy
+import numpy as np
 import pandas as pd
 from sklearn.cluster import KMeans
 
 df_allData=pd.read_csv('BlackFriday.csv')
 
-kmeans = KMeans(n_clusters=2, random_state=0).fit(df_allData)
+groupByUserData=df_allData.groupby(['User_ID'])
+#kmeans = KMeans(n_clusters=2, random_state=0).fit(df_allData)
